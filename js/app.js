df509ceda3d43ee70308520b8971daea11f517fc
wahid30 <md.wahidulalamjp2@gmail.com>
2022-09-23 11:19:46 +0600

    json data added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,77 @@
-console.log("wahid");
+const myJsonUrlFuc = () => {
+  fetch("../json/southPara.json")
+    .then((res) => res.json())
+    .then((datas) => displayMyData(datas));
+};
+
+const displayMyData = (datas) => {
+  datas.forEach((data) => {
+    // console.log(data);
+    const { house, recentUnit, previewsUnit, monthlyRent } = data;
+    // console.log(recentUnit);
+    const parentContainer = document.getElementById("container");
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div>
+          <h1 class="custom-heading">মেসার্স আলম শপিং সেন্টার</h1>
+          <p>বড় কুমিরা বাজার, সীতাকুন্ড, চট্টগ্রাম। মোবাইলঃ০১৮১৬-০২৫১৯৫</p>
+          <h3>অস্থায়ি ঘর বা দোকান ভাড়ার রসিদ</h3>
+        </div>
+        <div class="custom-flex">
+          <p>নং</p>
+          <p>
+            ভাড়াটিয়ার অংশ <br />
+            গৃহ বা হোল্ডিং নং
+          </p>
+          <p id="house-no" class="custom-filed">পুরাতন নিচেঃ এ-০১</p>
+        </div>
+        <div class="information-container">
+          <p>মালিক</p>
+          <div class="information">
+            <span>4500</span>
+            <span>-</span>
+            <span>4200</span>
+            <span>=</span>
+            <span>300</span>
+            <span>X</span>
+            <span>7</span>
+            <span>=</span>
+            <span>2100</span>
+          </div>
+        </div>
+        <div class="information-container">
+          <p>মাসিক ভাড়া</p>
+          <div class="information">
+            <span>4000</span>
+            <span>+</span>
+            <span>2100</span>
+            <span>=</span>
+            <span>6100</span>
+          </div>
+        </div>
+        <div class="information-container">
+          <p>অস্থায়ী ভাড়াটিয়ার নাম</p>
+          <div class="information">---</div>
+        </div>
+        <div class="information-container">
+          <p>রশিদ জমা দানের তারিখ</p>
+          <div class="information">30-09-2022</div>
+        </div>
+        <div class="information-container">
+          <p>মোট</p>
+          <div class="information">0</div>
+        </div>
+        <div class="information-container">
+          <p>তারিখ</p>
+          <div class="information">10-10-2022</div>
+        </div>
+        <div class="information-container">
+          <p>মালিক বা তাহাঁর কর্মচারী সাক্ষর</p>
+          <div class="information">...</div>
+        </div>
+    `;
+    parentContainer.appendChild(div);
+  });
+};
+
+myJsonUrlFuc();
